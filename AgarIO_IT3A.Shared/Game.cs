3c853a981c3cd360ff7dfa23d3a293d1b460231d
KOVUCZ <Hrdy.Adam@stud.vassboskovice.cz>
2024-04-10 12:27:10 +0200

    changes in game class

--- AgarIO_IT3A.Shared/Game.cs
+++ AgarIO_IT3A.Shared/Game.cs
@@ -2,9 +2,23 @@
 {
   public class Game
   {
+        public List<GameObject> gameObjects { get; }
+        
     public Game()
     {
-      
+      gameObjects = new List<GameObject>();
+           
+
     }
+
+    public void AddGameObject(Player player)
+        {
+            gameObjects.Add(player);
+        }
+
+    public void AddGameObject(Food food)
+        {
+            gameObjects.Add(food);
+        }
   }
 }
